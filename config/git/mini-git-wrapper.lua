aad0c124200b98c746108af9819e936552f7ede0
Igor Marta <mr.igor.marta@gmail.com>
2026-04-06 18:40:53 +0300

    Write to both stdout and stderr when available, mimicking git behavior
    Also improved subcommand detection.

--- config/git/mini-git-wrapper.lua
+++ config/git/mini-git-wrapper.lua
@@ -1,13 +1,16 @@
 local args = { unpack(arg, 1, #arg) }
-local index, subcmd = vim.iter(ipairs(args)):find(function(_, x) return string.match(x, '^%l.+$') end)
+local index, subcmd = vim.iter(ipairs(args))
+    :find(function(_, x) return string.match(x, '^%l+%-?%l+%-?%l+$') end)
+
 if vim.iter({ 'log', 'show' }):any(function(x) return x == subcmd end) then
   table.insert(args, index + 1, '--ext-diff')
 end
 table.insert(args, 1, 'git')
 
 local res = vim.system(args, { env = { DFT_WIDTH = '200' }, text = true }):wait()
-if not res.stderr or string.len(res.stderr) == 0 then
+if res.stdout then
   io.stdout:write(res.stdout)
-else
+end
+if res.stderr then
   io.stderr:write(res.stderr)
 end
