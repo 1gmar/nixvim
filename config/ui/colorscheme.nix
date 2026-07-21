2cc14e5bcd93835303198ddace46b6f6ee43c73e
Igor Marta <mr.igor.marta@gmail.com>
2026-07-21 18:00:53 +0300

    Install the neovim version of the solarized8 theme

--- config/ui/colorscheme.nix
+++ config/ui/colorscheme.nix
@@ -10,6 +10,14 @@
   };
   config = lib.mkIf config.color-scheme.enable {
     colorscheme = lib.mkDefault "solarized8";
-    extraPlugins = with pkgs.vimPlugins; [ vim-solarized8 ];
+    extraPlugins = with pkgs.vimPlugins; [
+      (vim-solarized8.overrideAttrs {
+        src = pkgs.fetchgit {
+          url = "https://codeberg.org/lifepillar/vim-solarized8/";
+          rev = "4ef98a611553a8093ae9e5af078ef6f94071e886";
+          hash = "sha256-I0/WeERQ8pazhd68p35/Pt1SF8LYW2c7i7hYgp+1hsE=";
+        };
+      })
+    ];
   };
 }
